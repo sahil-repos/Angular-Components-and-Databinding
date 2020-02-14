cbe41c6ab224fec77232ec47479ca430246cda16
Mohammed Sahil <mohammed.sahil@nagarro.com>
2020-02-14 14:57:59 +0530

    lifecyclehooks

--- src/app/server-elementt/server-elementt.component.ts
+++ src/app/server-elementt/server-elementt.component.ts
@@ -1,4 +1,12 @@
-import { Component, OnInit, Input, ViewEncapsulation } from '@angular/core';
+import { Component, OnInit, 
+  Input, ViewEncapsulation,
+   OnChanges, SimpleChanges, 
+   DoCheck, AfterContentInit, 
+   AfterContentChecked, 
+   AfterViewInit,
+   AfterViewChecked,
+   OnDestroy}
+    from '@angular/core';
 
 @Component({
   selector: 'app-server-elementt',
@@ -6,7 +14,13 @@ import { Component, OnInit, Input, ViewEncapsulation } from '@angular/core';
   styleUrls: ['./server-elementt.component.css'],
   encapsulation: ViewEncapsulation.Emulated,
 })
-export class ServerElementtComponent implements OnInit {
+export class ServerElementtComponent implements OnInit, OnChanges, 
+DoCheck, 
+AfterContentInit,
+AfterContentChecked,
+AfterViewInit,
+AfterViewChecked,
+OnDestroy	 {
 //@input to help parent component access child element's element
 //binding to custom prop , binding our own prop with @input decorator 
 //@input marks that prop can be set from outside 
@@ -15,9 +29,51 @@ export class ServerElementtComponent implements OnInit {
 //alias : to target other prop/name (here srvElement) , use the target  in @input() decorator, 
   @Input('srvElement') element: {type: string, name: string, content:string };
 
-  constructor() { }
+  constructor() {
 
-  ngOnInit() {
-  }
+    console.log('constructor called')
+   }
 
+ngOnChanges(changes: SimpleChanges){
+  console.log('ngOnchnges called');
+  console.log(changes);
+
+}
+
+ngOnInit() {
+  console.log('ngoninit called');
+
+}
+
+
+ngDoCheck(){
+  console.log('ngDoCheck called')
+
+}
+
+ngAfterContentInit(){
+  console.log('AfterContentInit called')
+
+}
+ngAfterContentChecked(){
+  console.log('ngaftercontetnchecked called')
+
+}
+
+ngAfterViewInit(){
+  console.log('ngAfterViewInit called')
+
+}
+
+ngAfterViewChecked(){
+  console.log('ngafterviewChcked called')
+
+}
+
+ngOnDestroy(){
+  console.log('ngOnDestroy called')
+
+}
+
+  
 }
