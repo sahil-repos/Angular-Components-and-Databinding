a27402e6565ccc4682de306a900c7cc0f295bc2e
Mohammed Sahil <mohammed.sahil@nagarro.com>
2020-02-14 12:11:27 +0530

    View Encapsulation

--- src/app/server-elementt/server-elementt.component.ts
+++ src/app/server-elementt/server-elementt.component.ts
@@ -1,9 +1,10 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, ViewEncapsulation } from '@angular/core';
 
 @Component({
   selector: 'app-server-elementt',
   templateUrl: './server-elementt.component.html',
-  styleUrls: ['./server-elementt.component.css']
+  styleUrls: ['./server-elementt.component.css'],
+  encapsulation: ViewEncapsulation.Emulated
 })
 export class ServerElementtComponent implements OnInit {
 //@input to help parent component access child element's element
